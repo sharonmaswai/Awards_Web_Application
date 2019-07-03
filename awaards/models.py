2ed686b7ab545e7c961173766a2c859db132645d
sharonmaswai <chepmaswai@gmail.com>
2019-07-03 17:00:32 +0300

    add new projects model

--- awaards/models.py
+++ awaards/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.core.validators import MaxValueValidator
+from django.dispatch import receiver
+from django.contrib.auth.models import User 
+from tinymce.models import HTMLField
+from url_or_relative_url_field.fields import URLOrRelativeURLField
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
+    profile_photo = models.ImageField(default='image.png',upload_to='profiles/')
+    bio = HTMLField(max_length=500,default='About me')
+    website = URLOrRelativeURLField() 
+    phone_number = models.CharField(max_length=10,default=000000)
+   
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+     
+    @receiver(post_save, sender=User) 
+    def save_profile(sender,instance,**kwargs):
+        instance.profile.save()  
+        
+    
+    @classmethod
+    def get_by_id(cls,id):
+        profile = Profile.objects.get(user = id)
+        return profile
+    
+    @classmethod
+    def filter_by_id(cls,id): 
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+    
+   
+    
+    def __str__(self):
+        return self.bio
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=20)
+    image = models.FilePathField(upload_to="projects/")
+    usability=models.IntegerField(default=0)
+    content=models.IntegerField(default=0)
+    link = URLOrRelativeURLField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
